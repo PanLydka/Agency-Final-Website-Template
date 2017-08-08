b434acf297621d13bab2b6aaf459e972ff49574a
PanLydka <kamillydka1@gmail.com>
2017-08-09 00:03:20 +0200

    import scroll js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import './../css/grid.css';
 import './../css/main.scss';
-import 'owl.carousel/dist/assets/owl.carousel.css';
 import $ from 'jquery';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import 'owl.carousel';
+
+//board
+import './board/scroll.js';
