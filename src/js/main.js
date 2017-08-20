9f67a8fc2683634c9a71b5c912ef8a91f07d2f75
PanLydka <kamillydka1@gmail.com>
2017-08-21 00:11:47 +0200

    opinions section contact secion

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 import _ from 'lodash';
-import './../css/grid.css';
-import './../css/main.scss';
 import $ from 'jquery';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
@@ -8,3 +6,6 @@ import 'owl.carousel';
 
 //board
 import './board/scroll.js';
+import './board/headerslider.js';
+import './../css/grid.css';
+import './../css/main.scss';
